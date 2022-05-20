9b29e60108b1e76368eaf377668559a01c81b4dd
Rodrigo Luque <rafarod8@gmail.com>
2022-05-20 08:27:00 +0200

    feat: server running and lint commands

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  roots: ['<rootDir>'],
+  testMatch: [
+    '**/__tests__/**/*.+(ts|js)',
+    '**/?(*.)+(spec|test).+(ts|js)',
+  ],
+  transform: {
+    '^.+\\.(ts)$': 'ts-jest',
+  },
+  testTimeout: 10000,
+};
---
+++ src/server.ts
@@ -0,0 +1,22 @@
+import express, { Application, Router } from "express";
+
+export class Server {
+  private app: Application;
+  private port: string;
+
+  constructor() {
+    this.port = process.env.PORT || "8080";
+    this.app = express();
+    const router = Router();
+    this.app.use(router);
+  }
+
+  async listen(): Promise<void> {
+    return new Promise((resolve) => {
+      this.app.listen(this.port, () => {
+        console.log(`Server running in ${this.app.get("env")}`);
+        resolve();
+      });
+    });
+  }
+}
